8aabb868a813b9714220e0cc4b61132818cd218f
manh20051151 <nguyenvietmanh1409@gmail.com>
2026-01-25 02:38:47 +0700

    add spring boot

---
+++ src/main/java/com/example/backendWVideos/BackendWVideosApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendWVideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWVideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendWVideos/BackendWVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendWVideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
